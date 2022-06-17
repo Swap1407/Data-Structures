8d24b0bfc0fb3a841d30b10f7b9f753f940397e5
Swapnil S Sawat <swapnilssawat@gmail.com>
2022-06-17 17:43:31 +0530

    added node for tree

---
+++ Node_tree.java
@@ -0,0 +1,11 @@
+package trees;
+
+public class Node_tree {
+	int data;
+	Node_tree left,right;
+	public Node_tree(int value) {
+		this.data = value;
+		this.left = null;
+		this.right = null;
+	}
+}
